64ee8cb26705913901d392897526042ca9c3ffc4
Ben Griffiths <mrbengriffiths@googlemail.com>
2012-07-09 00:30:32 +0100

    Work up to chapter two

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject caves "0.0.1-SNAPSHOT"
+  :description "The caves of clojure"
+  :url "http://stevelosh.com/blog/2012/07/caves-of-clojure-01/"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clojure-lanterna "0.9.0"]]
+  :plugins [[lein-swank "1.4.4"]]
+  :main caves.core)
